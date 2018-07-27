3b5a9250f14ea28a09ecf9fbf7472b031dac56e0
Jayda <ijerda@users.noreply.github.com>
2018-07-27 00:08:09 -0400

    update

--- assignments/termproject/scripts/accordion.js
+++ assignments/termproject/scripts/accordion.js
@@ -1,27 +1,7 @@
-$(document).ready(function() {
-  $(".set > a").on("click", function() {
-    if ($(this).hasClass("active")) {
-      $(this).removeClass("active");
-      $(this)
-        .siblings(".content")
-        .slideUp(800);
-      $(".set > a i")
-        .removeClass("fa-minus")
-        .addClass("fa-plus");
-    } else {
-      $(".set > a i")
-        .removeClass("fa-minus")
-        .addClass("fa-plus");
-      $(this)
-        .find("i")
-        .removeClass("fa-plus")
-        .addClass("fa-minus");
-      $(".set > a").removeClass("active");
-      $(this).addClass("active");
-      $(".content").slideUp(800);
-      $(this)
-        .siblings(".content")
-        .slideDown(800);
-    }
+  $(function () {
+      $("#accordion").accordion({
+          collapsible: true,
+          active: false
+      });
+      
   });
-});
